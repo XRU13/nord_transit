ce32a70f96df3ca6d33350cac3d9cfa3fc2d748e
XRU13 <aslanoff13@gmail.com>
2021-03-01 22:43:05 +0800

    add all

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    pass
+
+
+def services(request):
+    pass
+
+
+def assets(request):
+    pass
+
+
+def contact(request):
+    pass
